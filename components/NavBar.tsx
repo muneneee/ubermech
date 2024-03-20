1eb020244bc6eb5aa8c501f2cbfdb8a6433aa8b2
muneneee <lukekevin39@gmail.com>
2024-03-21 01:00:41 +0300

    Added booking and map directory

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -9,8 +9,8 @@ function NavBar() {
             <Image 
                 src='/logo.png' 
                 alt='logo'
-                width={120}
-                height={60}
+                width={80}
+                height={30}
             />
             <div className='hidden md:flex gap-6'>
                 <h2 className='hover:bg-gray-100 p-2 rounded-md cursor-pointer transition-all'>Home</h2>
