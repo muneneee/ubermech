247189922b162b1f401433cae1f87de7fb183ffe
muneneee <lukekevin39@gmail.com>
2024-03-21 00:17:11 +0300

    Added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import Image from "next/image";
-import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/" />
       <h2>Gbag na Jug</h2>
     </div>
   );
