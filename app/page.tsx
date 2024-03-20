b54c362c635be015c371296cd260aa816b712c04
muneneee <lukekevin39@gmail.com>
2024-03-20 23:41:45 +0300

    Added signin and signout functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
+      <UserButton afterSignOutUrl="/" />
       <h2>Gbag na Jug</h2>
     </div>
   );
